0c1b8717f05e8c96d3a45456f3849441342046b6
Naveen M <naveenm@Naveens-MacBook-Air.local>
2022-12-01 21:23:57 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+resource "azurerm_virtual_machine" "virtual_machine" {
+  name                             = var.name
+  location                         = var.location
+  resource_group_name              = var.resource_group_name
+  network_interface_ids            = [azurerm_network_interface.network_interface.id]
+  vm_size                          = var.vm_size
+  delete_os_disk_on_termination    = var.delete_os_disk_on_termination
+  delete_data_disks_on_termination = var.delete_data_disks_on_termination
+
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.storage_image_version
+  }
+
+  storage_os_disk {
+    name              = "${var.name}-disk"
+    caching           = var.caching
+    create_option     = var.create_option
+    managed_disk_type = var.managed_disk_type
+    os_type           = var.os_type
+  }
+
+  os_profile {
+    computer_name  = var.name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+    custom_data    = var.custom_data
+  }
+
+  dynamic "os_profile_linux_config" {
+    for_each = var.os_type == "Linux" ? [1] : []
+    content {
+      disable_password_authentication = var.disable_password_authentication
+    }
+  }
+
+  dynamic "os_profile_windows_config" {
+    for_each = var.os_type == "Windows" ? [1] : []
+    content {
+      timezone = var.timezone
+      provision_vm_agent = var.provision_vm_agent
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [
+    azurerm_network_interface.network_interface
+  ]
+}
+resource "azurerm_network_interface" "network_interface" {
+  name                = var.network_interface_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_configuration {
+    name                          = var.ip_name
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+  }
+}
+
+resource "azurerm_mssql_virtual_machine" "mssql_virtual_machine" {
+  virtual_machine_id               = azurerm_virtual_machine.virtual_machine.id
+  sql_license_type                 = "PAYG"
+  r_services_enabled               = true
+  sql_connectivity_port            = 1433
+  sql_connectivity_type            = "PRIVATE"
+  sql_connectivity_update_password = var.sql_connectivity_update_password
+  sql_connectivity_update_username = var.sql_connectivity_update_username
+}
---
+++ variables.tf
@@ -0,0 +1,122 @@
+variable "name" {
+     type = string
+     description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
+}
+variable "publisher" {
+     type = string
+     description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
+}
+variable "network_interface_name" {
+     type = string
+     description = "The name of the Network Interface. Changing this forces a new resource to be created."
+}
+variable "admin_username" {
+     type = string
+     description = "Specifies the name of the local administrator account."
+}
+variable "custom_data" {
+     type = string
+     description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
+}
+variable "private_ip_address_allocation" {
+     type = string
+     description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
+}
+variable "resource_group_name" {
+     type = string
+     description = "name of the resource group"
+}
+variable "sku" {
+     type = string
+     description = "Specifies the SKU of the image used to create the virtual machine. Examples: 18.04-LTS, 2019-Datacenter"
+}
+variable "location" {
+     type = string
+     description = "location of the resource group"
+}
+variable "offer" {
+     type = string
+     description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
+}
+variable "os_type" {
+     type = string
+     description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+}
+variable "subnet_id" {
+     type = string
+     description = "The ID of the Subnet where this Network Interface should be located in."
+}
+variable "vm_size" {
+     type = string
+     description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+}
+variable "managed_disk_type" {
+     type = string
+     description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+}
+variable "admin_password" {
+     type = string
+     description = "The password associated with the local administrator account."
+}
+
+variable "ip_name" {
+     type = string
+     description = "(optional) describe your variable"
+}
+
+variable "storage_image_version" {
+  type        = string
+  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
+  default     = "latest"
+}
+
+
+variable "caching" {
+  type        = string
+  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "ReadWrite"
+}
+
+variable "create_option" {
+  type        = string
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  default     = "FromImage"
+}
+
+
+variable "delete_os_disk_on_termination" {
+  type        = bool
+  description = " Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = true
+}
+
+variable "delete_data_disks_on_termination" {
+  type        = bool
+  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = true
+}
+
+variable "timezone" {
+     type = string
+     description = "(optional) describe your variable"
+     default = ""
+}
+
+variable "provision_vm_agent" {
+     type = bool
+     description = "(optional) describe your variable"
+     default = true
+}
+
+
+variable "sql_connectivity_update_password" {
+     type = string
+     description = "Name of password"
+     default = "Admin@1234"
+}
+
+variable "sql_connectivity_update_username" {
+     type = string
+     description = "Username of sql"
+     default = "sqllogin"
+}
