ba6b2aa9cbb100340acb2fb22d673df1957594f2
lalitha2407 <lalitha@bootlabstech.com>
2023-07-09 00:55:58 +0530

    code clean up

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
+# Getting existing Keyvault name to store credentials as secrets
 data "azurerm_key_vault" "key_vault" {
   name  = var.keyvault_name
   resource_group_name = var.resource_group_name
 }
 
+
+# Creates a Virtual machine
 resource "azurerm_virtual_machine" "virtual_machine" {
   name                             = var.name
   location                         = var.location
@@ -33,7 +36,6 @@ resource "azurerm_virtual_machine" "virtual_machine" {
     computer_name  = var.name
     admin_username = var.admin_username
     admin_password = random_password.password1.result
-    # custom_data    = var.custom_data
   }
 
   dynamic "os_profile_linux_config" {
@@ -60,6 +62,8 @@ resource "azurerm_virtual_machine" "virtual_machine" {
     azurerm_network_interface.network_interface
   ]
 }
+
+# Creates a private IP network interface card
 resource "azurerm_network_interface" "network_interface" {
   name                = "${var.name}-nic"
   location            = var.location
@@ -71,47 +75,36 @@ resource "azurerm_network_interface" "network_interface" {
   }
 }
 
+# Creates a MSSQL Virtual machine Image
 resource "azurerm_mssql_virtual_machine" "mssql_virtual_machine" {
   virtual_machine_id               = azurerm_virtual_machine.virtual_machine.id
-  sql_license_type                 = "PAYG"
-  r_services_enabled               = true
-  sql_connectivity_port            = 1433
-  sql_connectivity_type            = "PRIVATE"
-  sql_connectivity_update_password = random_password.password2.result
+  sql_license_type                 = var.sql_license_type
+  r_services_enabled               = var.r_services_enabled
+  sql_connectivity_port            = var.sql_connectivity_port
+  sql_connectivity_type            = var.sql_connectivity_type
+  sql_connectivity_update_password = random_password.password1.result
   sql_connectivity_update_username = var.sql_connectivity_update_username
 }
 
+# Creates a random password for SQL VM
 resource "random_password" "password1" {
-    length = 8
+    length = 12
     lower = true
-    min_lower = 1
-    min_numeric= 1
-    min_special= 1
-    min_upper= 1
+    min_lower = 6
+    min_numeric= 2
+    min_special= 2
+    min_upper= 2
     numeric = true
-    override_special = "_"
     special = true
     upper = true
     
 
 }
-resource "random_password" "password2" {
-    length = 8
-    lower = true
-    min_lower = 1
-    min_numeric= 1
-    min_special= 1
-    min_upper= 1
-    numeric = true
-    override_special = "_"
-    special = true
-    upper = true
-    
 
-}
+# Creates a secret to store DB credentials 
 resource "azurerm_key_vault_secret" "sqlvm_password" {
     name =  "${var.name}-sqlpwd"
-    value = random_password.password2.result
+    value = random_password.password1.result
     key_vault_id = data.azurerm_key_vault.key_vault.id
     
     depends_on = [ azurerm_mssql_virtual_machine.mssql_virtual_machine]
--- variables.tf
+++ variables.tf
@@ -1,82 +1,25 @@
+# VM
+variable "keyvault_name" {
+     type = string
+     description = "keyvault name to store the sql password"
+
+}
 variable "name" {
      type = string
      description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
 }
-variable "publisher" {
+variable "location" {
      type = string
-     description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
-}
-
-variable "admin_username" {
-     type = string
-     description = "Specifies the name of the local administrator account."
-}
-variable "disk_size_gb" {
-     type = number
-     description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
-}
-variable "private_ip_address_allocation" {
-     type = string
-     description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
-     default = "Dynamic"
+     description = "Specifies the Azure Region where the Virtual Machine exists. Changing this forces a new resource to be created."
 }
 variable "resource_group_name" {
      type = string
-     description = "name of the resource group"
-}
-variable "sku" {
-     type = string
-     description = "Specifies the SKU of the image used to create the virtual machine. Examples: 18.04-LTS, 2019-Datacenter"
-}
-variable "location" {
-     type = string
-     description = "location of the resource group"
-}
-variable "offer" {
-     type = string
-     description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
-}
-variable "os_type" {
-     type = string
-     description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
-}
-variable "subnet_id" {
-     type = string
-     description = "The ID of the Subnet where this Network Interface should be located in."
+     description = " Specifies the name of the Resource Group in which the Virtual Machine should exist."
 }
 variable "vm_size" {
      type = string
-     description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+     description = " Specifies the size of the Virtual Machine."
 }
-variable "managed_disk_type" {
-     type = string
-     description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
-}
-# variable "admin_password" {
-#      type = string
-#      description = "The password associated with the local administrator account."
-# }
-
-variable "storage_image_version" {
-  type        = string
-  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
-  default     = "latest"
-}
-
-
-variable "caching" {
-  type        = string
-  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
-  default     = "ReadWrite"
-}
-
-variable "create_option" {
-  type        = string
-  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
-  default     = "FromImage"
-}
-
-
 variable "delete_os_disk_on_termination" {
   type        = bool
   description = " Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
@@ -88,32 +31,105 @@ variable "delete_data_disks_on_termination" {
   description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
   default     = true
 }
+variable "publisher" {
+     type = string
+     description = "Specifies the publisher of the image used to create the virtual machine"
+}
+
+variable "sku" {
+     type = string
+     description = "Specifies the SKU of the image used to create the virtual machine."
+}
+
+variable "offer" {
+     type = string
+     description = "Specifies the offer of the image used to create the virtual machine"
+}
+
+variable "storage_image_version" {
+  type        = string
+  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
+  default     = "latest"
+}
+variable "caching" {
+  type        = string
+  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "ReadWrite"
+}
+
+variable "create_option" {
+  type        = string
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  default     = "FromImage"
+}
+variable "os_type" {
+     type = string
+     description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+}
+variable "managed_disk_type" {
+     type = string
+     description = " Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+}
+variable "disk_size_gb" {
+     type = number
+     description = " Specifies the size of the data disk in gigabytes."
+}
+variable "admin_username" {
+     type = string
+     description = "Specifies the name of the local administrator account."
+}
+variable "disable_password_authentication" {
+     type = string
+     description = "Specifies whether password authentication should be disabled. If set to false, an admin_password must be specified."
+     default = "true"
+}
+variable "provision_vm_agent" { 
+     type = bool
+     description = "Should the Azure Virtual Machine Guest Agent be installed on this Virtual Machine? Defaults to false."
+     default = true
+}
+variable "subnet_id" {
+     type = string
+     description = "The ID of the Subnet where this Network Interface should be located in."
+}
+variable "private_ip_address_allocation" {
+     type = string
+     description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
+     default = "Dynamic"
+}
+
 
 variable "timezone" {
      type = string
-     description = "(optional) describe your variable"
+     description = " Specifies the time zone of the virtual machine"
      default = ""
 }
-
-variable "provision_vm_agent" { 
-     type = bool
-     description = "(optional) describe your variable"
-     default = true
-}
-
-
-variable "disable_password_authentication" {
-     type = string
-     description = "Name of password"
-     default = "true"
-}
-
+# MSSQL VM Image
 variable "sql_connectivity_update_username" {
      type = string
-     description = "Username of sql"
+     description = "The SQL Server sysadmin login to create."
 }
-variable "keyvault_name" {
+
+variable "sql_license_type" {
      type = string
-     description = "keyvault name"
+     description = "The SQL Server license type. Possible values are AHUB (Azure Hybrid Benefit), DR (Disaster Recovery), and PAYG (Pay-As-You-Go)."
+     default = "PAYG"
+
+}
+variable "r_services_enabled" { 
+     type = bool
+     description = "Should R Services be enabled?"
+     default = true
+}
+variable "sql_connectivity_port" {
+     type = number
+     description = "The SQL Server license type. Possible values are AHUB (Azure Hybrid Benefit), DR (Disaster Recovery), and PAYG (Pay-As-You-Go)."
+     default = 1433
+
+}
+variable "sql_connectivity_type" {
+     type = string
+     description = "The SQL Server license type. Possible values are AHUB (Azure Hybrid Benefit), DR (Disaster Recovery), and PAYG (Pay-As-You-Go)."
+     default = "PRIVATE"
 
 }
