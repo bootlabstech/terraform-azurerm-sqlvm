c13184081f613523fef182f3214fde19070793a3
Bhargava Naidu <bhargava@bootlabstech.com>
2025-05-15 17:44:41 +0530

    changed the config for syncing

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
-data "azurerm_key_vault" "key_vault" {
-  name  = var.keyvault_name
-  resource_group_name = var.resource_group_name
-}
-
+# Creates a Virtual machine
 resource "azurerm_virtual_machine" "virtual_machine" {
   name                             = var.name
   location                         = var.location
   resource_group_name              = var.resource_group_name
   network_interface_ids            = [azurerm_network_interface.network_interface.id]
   vm_size                          = var.vm_size
-  
   delete_os_disk_on_termination    = var.delete_os_disk_on_termination
   delete_data_disks_on_termination = var.delete_data_disks_on_termination
 
@@ -26,14 +21,13 @@ resource "azurerm_virtual_machine" "virtual_machine" {
     create_option     = var.create_option
     managed_disk_type = var.managed_disk_type
     os_type           = var.os_type
-    disk_size_gb = var.disk_size_gb
+    disk_size_gb      = var.disk_size_gb
   }
 
   os_profile {
     computer_name  = var.name
     admin_username = var.admin_username
-    admin_password = random_password.password1.result
-    # custom_data    = var.custom_data
+    admin_password = random_password.password.result
   }
 
   dynamic "os_profile_linux_config" {
@@ -46,7 +40,7 @@ resource "azurerm_virtual_machine" "virtual_machine" {
   dynamic "os_profile_windows_config" {
     for_each = var.os_type == "Windows" ? [1] : []
     content {
-      timezone = var.timezone
+      timezone           = var.timezone
       provision_vm_agent = var.provision_vm_agent
     }
   }
@@ -60,6 +54,8 @@ resource "azurerm_virtual_machine" "virtual_machine" {
     azurerm_network_interface.network_interface
   ]
 }
+
+# Creates a private IP network interface card
 resource "azurerm_network_interface" "network_interface" {
   name                = "${var.name}-nic"
   location            = var.location
@@ -71,48 +67,144 @@ resource "azurerm_network_interface" "network_interface" {
   }
 }
 
+# Creates a MSSQL Virtual machine Image in the VM
 resource "azurerm_mssql_virtual_machine" "mssql_virtual_machine" {
   virtual_machine_id               = azurerm_virtual_machine.virtual_machine.id
-  sql_license_type                 = "PAYG"
-  r_services_enabled               = true
-  sql_connectivity_port            = 1433
-  sql_connectivity_type            = "PRIVATE"
-  sql_connectivity_update_password = random_password.password2.result
+  sql_license_type                 = var.sql_license_type
+  r_services_enabled               = var.r_services_enabled
+  sql_connectivity_port            = var.sql_connectivity_port
+  sql_connectivity_type            = var.sql_connectivity_type
+  sql_connectivity_update_password = random_password.password1.result
   sql_connectivity_update_username = var.sql_connectivity_update_username
+
+  storage_configuration {
+    disk_type             = var.data_disk_type
+    storage_workload_type = var.storage_workload_type
+    data_settings {
+      default_file_path = var.default_file_path
+      luns              = [0]
+    }
+
+  }
+}
+# Creates additional Datadisk for DB
+resource "azurerm_managed_disk" "disk" {
+  name                 = "${var.name}-dbdataDisk"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = var.managed_disk_type
+  create_option        = var.datadisk_create_option
+  disk_size_gb         = var.data_disk_size_gb
 }
 
+# Attaches datadisk to VM
+resource "azurerm_virtual_machine_data_disk_attachment" "disk_attachment" {
+  managed_disk_id    = azurerm_managed_disk.disk.id
+  virtual_machine_id = azurerm_virtual_machine.virtual_machine.id
+  lun                = var.lun
+  caching            = var.data_disk_caching
+}
+
+# Creates Network Security Group NSG for Virtual Machine
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.name}-nsg"
+  location            = azurerm_virtual_machine.virtual_machine.location
+  resource_group_name = azurerm_virtual_machine.virtual_machine.resource_group_name
+}
+
+
+# Creates Network Security Group Default Rules for Virtual Machine
+resource "azurerm_network_security_rule" "nsg_rules" {
+  for_each                    = var.nsg_rules
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_address_prefix       = each.value.source_address_prefix
+  source_port_range           = each.value.source_port_range
+  destination_address_prefix  = each.value.destination_address_prefix
+  destination_port_range      = each.value.destination_port_range
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_virtual_machine.virtual_machine.resource_group_name
+}
+
+
+# Creates association (i.e) adds NSG to the NIC
+resource "azurerm_network_interface_security_group_association" "security_group_association" {
+  network_interface_id      = azurerm_network_interface.network_interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Getting existing recovery_services_vault to add vm as a backup item 
+data "azurerm_recovery_services_vault" "services_vault" {
+  name                = var.recovery_services_vault_name
+  resource_group_name = var.services_vault_resource_group_name
+}
+
+# Getting existing Backup Policy for Virtual Machine
+data "azurerm_backup_policy_vm" "policy" {
+  name                = "VM-backup-policy"
+  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+  resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
+}
+
+# Creates Backup protected Virtual Machine
+resource "azurerm_backup_protected_vm" "backup_protected_vm" {
+  resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
+  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+  source_vm_id        = azurerm_virtual_machine.virtual_machine.id
+  backup_policy_id    = data.azurerm_backup_policy_vm.policy.id
+  depends_on = [
+    azurerm_virtual_machine.virtual_machine
+  ]
+}
+
+# Getting existing Keyvault name to store sql vm credentials as secrets
+data "azurerm_key_vault" "key_vault" {
+  name                = var.keyvault_name
+  resource_group_name = var.resource_group_name
+}
+
+# Creates a random string password for vm default user
+resource "random_password" "password" {
+  length      = 12
+  lower       = true
+  min_lower   = 6
+  min_numeric = 2
+  min_special = 2
+  min_upper   = 2
+  numeric     = true
+  special     = true
+  upper       = true
+
+}
+
+# Creates a random password for SQL VM
 resource "random_password" "password1" {
-    length = 8
-    lower = true
-    min_lower = 1
-    min_numeric= 1
-    min_special= 1
-    min_upper= 1
-    numeric = true
-    override_special = "_"
-    special = true
-    upper = true
-    
-
+  length      = 12
+  lower       = true
+  min_lower   = 6
+  min_numeric = 2
+  min_special = 2
+  min_upper   = 2
+  numeric     = true
+  special     = true
+  upper       = true
 }
-resource "random_password" "password2" {
-    length = 8
-    lower = true
-    min_lower = 1
-    min_numeric= 1
-    min_special= 1
-    min_upper= 1
-    numeric = true
-    override_special = "_"
-    special = true
-    upper = true
-    
 
+# Creates a secret to store DB credentials 
+resource "azurerm_key_vault_secret" "sqldb_password" {
+  name         = "${var.name}-sqlpwd"
+  value        = random_password.password1.result
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+
+  depends_on = [azurerm_mssql_virtual_machine.mssql_virtual_machine]
 }
-resource "azurerm_key_vault_secret" "sqlvm_password" {
-    name =  "${var.name}-sqlpwd"
-    value = random_password.password2.result
-    key_vault_id = data.azurerm_key_vault.key_vault.id
-    
-    depends_on = [ azurerm_mssql_virtual_machine.mssql_virtual_machine]
+# Creates a secret to store VM credentials 
+resource "azurerm_key_vault_secret" "vm_password" {
+  name         = "${var.name}-vmpwd"
+  value        = random_password.password.result
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+
 }
--- variables.tf
+++ variables.tf
@@ -1,119 +1,222 @@
+# VM
 variable "name" {
-     type = string
-     description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
-}
-variable "publisher" {
-     type = string
-     description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
-}
-
-variable "admin_username" {
-     type = string
-     description = "Specifies the name of the local administrator account."
-}
-variable "disk_size_gb" {
-     type = number
-     description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
-}
-variable "private_ip_address_allocation" {
-     type = string
-     description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
-     default = "Dynamic"
-}
-variable "resource_group_name" {
-     type = string
-     description = "name of the resource group"
-}
-variable "sku" {
-     type = string
-     description = "Specifies the SKU of the image used to create the virtual machine. Examples: 18.04-LTS, 2019-Datacenter"
+  type        = string
+  description = "Specifies the name of the Virtual Machine. No uppercase and less than 15 characters"
 }
 variable "location" {
-     type = string
-     description = "location of the resource group"
+  type        = string
+  description = "Specifies the Azure Region where the Virtual Machine exists. Must be one of the following Central India or South India."
 }
-variable "offer" {
-     type = string
-     description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
-}
-variable "os_type" {
-     type = string
-     description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
-}
-variable "subnet_id" {
-     type = string
-     description = "The ID of the Subnet where this Network Interface should be located in."
+variable "resource_group_name" {
+  type        = string
+  description = " Specifies the name of the Resource Group in which the Virtual Machine should exist."
 }
 variable "vm_size" {
-     type = string
-     description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+  type        = string
+  description = "Specifies the size of the Virtual Machine. See also Azure documentation for all options."
 }
-variable "managed_disk_type" {
-     type = string
-     description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+variable "delete_os_disk_on_termination" {
+  type        = bool
+  description = "Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = true
 }
-# variable "admin_password" {
-#      type = string
-#      description = "The password associated with the local administrator account."
-# }
+variable "delete_data_disks_on_termination" {
+  type        = bool
+  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
+  default     = false
+}
 
+# storage_image_reference
+variable "publisher" {
+  type        = string
+  description = "Specifies the publisher of the image used to create the virtual machine."
+  default     = "MicrosoftSQLServer"
+}
+variable "offer" {
+  type        = string
+  description = "Specifies the offer of the image used to create the virtual machine."
+}
+variable "sku" {
+  type        = string
+  description = "Specifies the SKU of the image used to create the virtual machine."
+  default     = "Standard"
+}
 variable "storage_image_version" {
   type        = string
-  description = "Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
+  description = "Specifies the version of the image used to create the virtual machine."
   default     = "latest"
 }
 
-
+# storage_os_disk
 variable "caching" {
   type        = string
   description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
   default     = "ReadWrite"
 }
-
 variable "create_option" {
   type        = string
   description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
   default     = "FromImage"
 }
-
-
-variable "delete_os_disk_on_termination" {
-  type        = bool
-  description = " Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed? Defaults to false."
-  default     = true
+variable "managed_disk_type" {
+  type        = string
+  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+}
+variable "os_type" {
+  type        = string
+  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+}
+variable "disk_size_gb" {
+  type        = number
+  description = "Specifies the size of the OS Disk in gigabytes."
 }
 
-variable "delete_data_disks_on_termination" {
-  type        = bool
-  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed? Defaults to false."
-  default     = true
-}
+# os_profile
+variable "admin_username" {
+  type        = string
+  description = "Specifies the name of the local administrator account."
 
-variable "timezone" {
-     type = string
-     description = "(optional) describe your variable"
-     default = ""
 }
-
-variable "provision_vm_agent" { 
-     type = bool
-     description = "(optional) describe your variable"
-     default = true
-}
-
 
 variable "disable_password_authentication" {
-     type = string
-     description = "Name of password"
-     default = "true"
+  type        = bool
+  description = "Specifies whether password authentication should be disabled. If set to false, an admin_password must be specified."
+  default     = false
+}
+variable "timezone" {
+  type        = string
+  description = "Specifies the time zone of the virtual machine, the possible values are defined here. Changing this forces a new resource to be created."
+  default     = ""
+}
+variable "provision_vm_agent" {
+  type        = bool
+  description = "Should the Azure Virtual Machine Guest Agent be installed on this Virtual Machine? Defaults to false."
+  default     = true
 }
 
-variable "sql_connectivity_update_username" {
-     type = string
-     description = "Username of sql"
+# network_interface
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet where this Network Interface should be located in."
 }
+variable "private_ip_address_allocation" {
+  type        = string
+  description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static."
+  default     = "Dynamic"
+}
+
+# MSSQL VM
+variable "sql_license_type" {
+  type        = string
+  description = "The SQL Server license type. Possible values are AHUB (Azure Hybrid Benefit), DR (Disaster Recovery), and PAYG (Pay-As-You-Go). Changing this forces a new resource to be created."
+  default     = "PAYG"
+
+}
+variable "r_services_enabled" {
+  type        = bool
+  description = "Should R Services be enabled?"
+  default     = true
+}
+variable "sql_connectivity_port" {
+  type        = number
+  description = "The SQL Server port. Defaults to 1433."
+  default     = 1433
+
+}
+variable "sql_connectivity_type" {
+  type        = string
+  description = "The connectivity type used for this SQL Server. Possible values are LOCAL, PRIVATE and PUBLIC. Defaults to PRIVATE."
+  default     = "PRIVATE"
+
+}
+variable "sql_connectivity_update_username" {
+  type        = string
+  description = "The SQL Server sysadmin login to create."
+}
+
+# storage_configuration
+variable "data_disk_type" {
+  type        = string
+  description = "The type of disk configuration to apply to the SQL Server. Valid values include NEW, EXTEND, or ADD."
+  default     = "NEW"
+}
+variable "storage_workload_type" {
+  type        = string
+  description = "The type of storage workload. Valid values include GENERAL, OLTP, or DW."
+  default     = "GENERAL"
+}
+variable "default_file_path" {
+  type        = string
+  description = "The SQL Server default path"
+  default     = "F:/data"
+}
+
+# azurerm_network_security_rule
+variable "nsg_rules" {
+  type = map(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_address_prefix      = string
+    source_port_range          = string
+    destination_address_prefix = string
+    destination_port_range     = string
+  }))
+  default = {
+    "https" = {
+      access                     = "Allow"
+      destination_address_prefix = "*"
+      destination_port_range     = "443"
+      direction                  = "Inbound"
+      name                       = "allow-https"
+      priority                   = 100
+      protocol                   = "Tcp"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+    }
+  }
+
+}
+
+# Datadisk azurerm_managed_disk
+variable "datadisk_create_option" {
+  type        = string
+  description = " The method to use when creating the managed disk. Changing this forces a new resource to be created. Possible values include: Empty,Import,ImportSecure,Copy,FromImage,Restore, Upload"
+  default     = "Empty"
+}
+variable "data_disk_size_gb" {
+  type        = number
+  description = "Specifies the size of the managed disk to create in gigabytes. If create_option is Copy or FromImage, then the value must be equal to or greater than the source's size. The size can only be increased."
+}
+
+# Datadisk attachment
+variable "lun" {
+  type        = number
+  description = "The Logical Unit Number of the Data Disk, which needs to be unique within the Virtual Machine. Changing this forces a new resource to be created."
+  default     = 0
+}
+variable "data_disk_caching" {
+  type        = string
+  description = "Specifies the caching requirements for this Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "ReadWrite"
+
+}
+
+# azurerm_recovery_services_vault
+variable "recovery_services_vault_name" {
+  type        = string
+  description = "name of the recover service vault"
+}
+variable "services_vault_resource_group_name" {
+  type        = string
+  description = "name of resource group where the recovery service vault reside in"
+}
+
+# Keyvault
 variable "keyvault_name" {
-     type = string
-     description = "keyvault name"
+  type        = string
+  description = "keyvault name to store the sql password"
 
 }
